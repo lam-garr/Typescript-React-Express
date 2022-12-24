c111d3077fce347a2b1a8e902c5bd26867b339b4
Garrett <lamgarr@outlook.com>
2022-12-23 19:59:19 -0800

    Add JWT authentication log in

--- server/controllers/apiController.ts
+++ server/controllers/apiController.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import User from "../models/user";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export function GET_index(req: Request, res:Response){
     res.json({message:"Hello World"});
@@ -19,5 +20,16 @@ export async function POST_sign_up(req: Request, res: Response, next: NextFuncti
             res.json({message:"Success"});
         }
     })
+}
+
+export async function POST_log_in(req: Request, res: Response, next: NextFunction){
+    const user = await User.findOne({username:req.body.username});
+
+    if(user &&(await bcrypt.compare(req.body.password, user.password))){
+        const token = jwt.sign(user._id, `${process.env.SECRET}`, {expiresIn: "15m"});
+        res.json({accessToken:token});
+    }else{
+        res.status(400).json({message:"Erorr"});
+    }
 }
 
