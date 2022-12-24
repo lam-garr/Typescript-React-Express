fceed3b6fbc04829924d388df112c08571edc1b5
Garrett <lamgarr@outlook.com>
2022-12-23 18:58:13 -0800

    Restructure files and set up api call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
+
+interface apiData {
+  message: string | undefined
+}
 
 function App() {
+
+  const[ data, setdata ] = useState<any>(null);
+
+  useEffect(()=>{
+    async function fetchAPI() {
+      let response = await fetch("/api");
+      console.log(response);
+      const jsonResponse = await response.json();
+      console.log(jsonResponse)
+      setdata(jsonResponse.message);
+    }
+    fetchAPI();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data}
     </div>
   );
 }
