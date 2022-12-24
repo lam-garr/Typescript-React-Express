624a0808f3fa45b4d8aa9ee80f5c0b3e6c408df1
Garrett <lamgarr@outlook.com>
2022-12-23 16:19:56 -0800

    Add controller for api route

--- server/controllers/apiController.ts
+++ server/controllers/apiController.ts
@@ -1,1 +1,23 @@
 import express, { Request, Response, NextFunction } from "express";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+
+export function GET_index(req: Request, res:Response){
+    res.json({message:"Hello World"});
+}
+
+export async function POST_sign_up(req: Request, res: Response, next: NextFunction){
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const user = new User({
+        username: req.body.username,
+        password: hashedPassword
+    }).save(err =>{
+        if(err){
+            return next(err);
+        }else{
+            res.json({message:"Success"});
+        }
+    })
+}
+
