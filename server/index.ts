e2e78fafd7f5c506f87b7af4e693f58f4ce73ba2
Garrett <lamgarr@outlook.com>
2022-12-23 16:51:51 -0800

    Add mongoose connection

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,13 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 dotenv.config();
+
+const mongoDB = `${process.env.MONGODB}`;
+mongoose.connect(mongoDB);
+const db = mongoose.connection;
+db.on("error", console.log.bind(console, "db connection error"));
 
 const app: Express = express();
 
